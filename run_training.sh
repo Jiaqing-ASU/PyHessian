5e7ccd56cc3bd340d6e14c037b825efb5ef8cb92
Jiaqing-ASU <jchen501@asu.edu>
2023-08-01 16:04:12 -0700

    set random seeds
    Signed-off-by: Jiaqing-ASU <jchen501@asu.edu>

--- run_training.sh
+++ run_training.sh
@@ -1,7 +1,16 @@
 # python3 training.py --depth=20 --batch-norm ---residual
-python3 training.py --depth=20 --no-batch-norm --residual
-python3 training.py --depth=20 --batch-norm --no-residual
+# python3 training.py --depth=20 --no-batch-norm --residual
+# python3 training.py --depth=20 --batch-norm --no-residual
 
-# python3 training.py --depth=20 --batch-norm ---residual
-python3 training.py --depth=20 --no-batch-norm --residual
-python3 training.py --depth=20 --batch-norm --no-residual
+# python3 training.py --depth=38 --batch-norm ---residual
+# python3 training.py --depth=38 --no-batch-norm --residual
+# python3 training.py --depth=38 --batch-norm --no-residual
+
+python3 training.py --depth=20 --batch-norm ---residual --seed=123
+python3 training.py --depth=20 --batch-norm --no-residual --seed=123
+
+python3 training.py --depth=20 --batch-norm ---residual --seed=123456
+python3 training.py --depth=20 --batch-norm --no-residual --seed=123456
+
+python3 training.py --depth=20 --batch-norm ---residual --seed=2023
+python3 training.py --depth=20 --batch-norm --no-residual --seed=2023
